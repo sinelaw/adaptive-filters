aa0d37b212d19792a02e13a2e0aad909815a1553
Noam <noam@duapov.(none)>
2010-06-14 16:36:38 +0300

    fixed bugs in q2

--- bonus/question2.m
+++ bonus/question2.m
@@ -1,38 +1,47 @@
 function question2()
 
 % Print the weights for a filter of order 5
-p5 = y_autocorellation_vector(5);
-R5 = toeplitz(p5);
-w5 = R5 / p5;
+r5 = y_autocorellation_vector(0,4)
+R5 = toeplitz(r5);
+
+p5 = y_autocorellation_vector(1,5);
+w5 = R5 \ p5;
 display('Weights for filter of order 5:');
 display(w5);
 
 
 % Create a 400 sample input 
 L = 400;
-x = rand(L,1);
+x = randn(L,1);
 b = [1,-0.1];
 a = [-0.4];
 y = filter(b, a, x);
 
-i = 1;
+subplot(2,3,1);
+plot(y);
+subplot(2,3,2);
+plot(x);
+i = 3;
 for filter_order = [1,2,5,15]
     % Build input matrices
     [ U, N ] = input_matrix(y, filter_order);
-    p = y_autocorellation_vector(filter_order);
-    R = toeplitz(p);
-    
+    r = y_autocorellation_vector(0,filter_order-1);    
+    R = toeplitz(r);
+    p = y_autocorellation_vector(1,filter_order);
+
     % Perform filtering
     [z, w] = weiner(U, R, p, false);
     
     % calculate error
-    size(z)
-    size(y)
-    e = abs(z - y).^2
-    
+    y_trunc = y(filter_order:length(y)-1, :);
+    e = abs(z' - y_trunc).^2;
     % plot
     subplot(2,3,i);
-    plot(e);
+    hold off;
+    plot(z);
+    hold on;
+    plot(y_trunc, 'r');
+    hold off;
     i = i + 1;
 end
 
@@ -43,12 +52,12 @@ function [ U, N ] = input_matrix(y, filter_order)
     N = length(y) - M;
     U = [];
     for i = 1 : N
-        U = [U, y(i+M:-1:i)];
+        U = [U, y(i+M-1:-1:i)];
     end
 end
 
-function [ p ] = y_autocorellation_vector(length)
-    p = arrayfun(@(x) y_autocorellation(x), 1:length);
+function [ p ] = y_autocorellation_vector(start, stop)
+    p = arrayfun(@(x) y_autocorellation(x), (start:stop)');
 end
 
 function [ r ] = y_autocorellation(l)
@@ -56,7 +65,9 @@ function [ r ] = y_autocorellation(l)
     b1 = -0.1;
 
     r0 = (1 + (a1 + b1)^2) / (1 - a1^2);
-
-    r = a1^l * r0 + b1 * a1^(l-1);
-
+    if (l == 0)
+        r = r0;
+    else
+        r = a1^l * r0 + b1 * a1^(l-1);
+    end
 end
